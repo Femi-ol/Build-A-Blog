b75b493849c90e3ff645abcfe06392cd6dc76aba
Femi-ol <femi.oladosu@gmail.com>
2019-02-15 10:05:10 +0100

    updateed

--- Build-A-Blog-master/app.py
+++ Build-A-Blog-master/app.py
@@ -3,6 +3,11 @@ from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
+# theTitle = " " 
+# thePost = " "
+
+allPosts = []
+
 @app.route('/')
 def hello():
 
@@ -16,43 +21,43 @@ def createBlog():
 	if request.method == 'GET':
 	 	# Send page with form for blog post
 		return render_template("writePost.html")
+
 	elif request.method == 'POST':
+	 	
 	 	# accept entry into form above
+	 	# create a variable to store images
 	 	
 	 	theTitle = request.form['myTitle']
 	 	thePost = request.form['myPost']
-	 	# thePost = request.form['myPost']
-	 	theImage = 'You'
-	 	return "Hi"
-	# 	theImage = request.form['myImage']
 
+	 	# use dictionary to store multiple posts
+	 	new_post = {
 
-	 	return """
-			<html>
-				<head>
-					<title>
-						Hello World!
-					</title>
-				</head>
-				<body>
-				<div>
-					""" + theTile + """
-					</div>
-				<div>
-					""" + thePost + """
-					</div>
-				<div>
-					""" + theImage + """
-					</div>
-				</body>
-			</html>"""
+		'pTitle': request.form['myTitle'],
+		'pPost': request.form['myPost']
+		}
+
+	 	allPosts.append(new_post)
+
+	 	# Give poster confirmation page
+	 	return render_template('confirm.html')
+
 	else:
 	 	return "Very bad request"
 
 
-@app.route('/readBlog')
+@app.route('/readBlog', methods=['GET'])
 def readBlog():
- 	return "This is the  blog page"
+
+	buildContent = ""
+
+	# scroll through dictionary and print out posts.
+	for inPost in allPosts:
+		buildContent += "<h2>" + inPost['pTitle'] + "</h2>"
+		buildContent += "<div>" + inPost['pPost'] + "</div> <br /><br /><br />"
+		
+	return render_template('blogPost.html', buildContent = buildContent)
+
 
 
 if __name__ == '__main__':
